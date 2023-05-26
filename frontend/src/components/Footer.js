62ff3fe7c4232b93a6452ddabb1b50285345a7e8
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-05-26 15:07:46 -0500

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import {Container, Row, Col } from 'react-bootstrap'
 
-export default function Header(){
+export default function Footer(){
     return (
         <footer>
-            <Container>
+            <Container fluid className = "bg-dark text-light">
                 <Row>
                     <Col className = 'text-center py-3'>
                         Copyright &copy; Joseph Hansen
