6e50467877caa27a31db724abc134ed07316ffc9
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-05-30 13:09:24 -0500

    models

---
+++ backend/config/models/userModel.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+const userSchema = new mongoose.Schema({
+    name:{type: String, required: true},
+    email:{type: String, required: true, unique: true},
+    password:{type: String, required: true, unique: false},
+    isAdmin:{type: Boolean, required: true, default:false},
+},{timestamps: true})
+
+const User = mongoose.model("User", userSchema)
+export default User
