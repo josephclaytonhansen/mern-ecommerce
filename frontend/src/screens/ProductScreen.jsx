cd6c51385021a8374dfcf8fc95b131dc0e9e93e6
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-06-01 12:08:07 -0500

    add to cart

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
+import { useState } from 'react'
 import Product from '../components/Product'
+import { useDispatch } from 'react-redux'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
-import {Container, Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap'
-import { useParams, Link } from 'react-router-dom'
+import {Container, Row, Col, Image, ListGroup, Card, Button, ListGroupItem, Form } from 'react-bootstrap'
+import { useParams, Link, useHistory } from 'react-router-dom'
 import Rating from '../components/Rating'
 import { useGetProductDetailsQuery } from '../slices/productsApiSlice'
+import { addToCart } from '../slices/cartSlice'
 
 
 export default function ProductScreen(){
     
     const { id: productId } = useParams()
     const { data:product, isLoading, error } = useGetProductDetailsQuery(productId)
+
+    const [qty, setQty] = useState(1)
+
+    const dispatch = useDispatch()
+    const history = useHistory()
+
+
+    const addToCartHandler = () => {
+        dispatch(addToCart({...product, qty}))
+        history.push('/cart')
+    }
 
     return (
         <>
@@ -41,8 +55,20 @@ export default function ProductScreen(){
                                     <Col>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</Col>
                                 </Row>
                             </ListGroup.Item>
+
+                            {product.countInStock > 0 && (
+                                <ListGroup.Item>
+                                    <Row><Col>Qty</Col><Col>
+                                    <Form.Control as='select' value={qty} onChange={(e) => setQty(Number(e.target.value))}>
+                                        {[...Array(product.countInStock).keys()].map((x) => (<option key={x+1} value={x+1}>{x+1}</option>))}
+                                    </Form.Control>
+                                    </Col></Row>
+                                </ListGroup.Item>
+                            )}
+
                             <ListGroup.Item>
-                                <Button className='btn-block' type='button' disabled = {product.countInStock <= 0} >Add to Cart</Button>
+                                <Button className='btn-block' type='button' onClick={addToCartHandler}
+                                 disabled = {product.countInStock <= 0} >Add to Cart</Button>
                             </ListGroup.Item>
                     </ListGroup>
                 </Col>
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -2,10 +2,48 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : { cartItems: [] }
 
+const addDecimals = (num) => {
+    return Number((Math.round(num * 100) / 100).toFixed(2))
+}
+
+function addToCartHelper(state, action){
+    const item = action.payload
+    const inCart = state.cartItems.find((p) => p._id === item._id)
+
+    if (inCart) {
+        state.cartItems = state.cartItems.map((p) => p._id === inCart._id ? item : p)
+    } else {
+        state.cartItems = [...state.cartItems, item]
+    }
+    state.totalPrice = addDecimals(itemsPrice(state) + shippingPrice(state) + taxPrice(state))
+
+    localStorage.setItem('cart', JSON.stringify(state))
+
+}
+
+function itemsPrice(state){
+    state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + (item.price * item.qty), 0))
+    return state.itemsPrice
+}
+
+//replace later with the ShipEngine API
+function shippingPrice(state){
+    state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10)
+    return state.shippingPrice
+}
+//replace later with the Precisely API (https://developer.precisely.com/apis/localtax)
+function taxPrice(state){
+    state.taxPrice = addDecimals(Number(0.15 * state.itemsPrice))
+    return state.taxPrice
+}
+
 const cartSlice = createSlice({
     name: 'cart',
     initialState,
-    reducers: {}
+    reducers: {
+        addToCart: (state, action) => addToCartHelper(state, action)
+    }
 })
 
 export default cartSlice.reducer
+export const { addToCart } = cartSlice.actions
