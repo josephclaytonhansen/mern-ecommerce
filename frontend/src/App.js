edbb1e4fbd4d9adb089f8b40769e64e062d3b63c
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-06-03 19:42:27 -0500

    user login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,16 @@ import React from 'react'
 
 import { BrowserRouter as Router, Route, Switch, withRouter  } from 'react-router-dom'
 
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 import {Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
-
+import LoginScreen from './screens/LoginScreen'
 
 function App() {
   return (
@@ -21,10 +24,12 @@ function App() {
           <Route path="/product/:id" component={withRouter(ProductScreen)} />
           <Route path="/cart" component={withRouter(CartScreen)} />
           <Route path="/" component={withRouter(HomeScreen)} exact index={true} />
+          <Route path="/login" component={(LoginScreen)} />
           </Switch>
         </Container>
         </main>
         <Footer />
+        <ToastContainer />
 
     </Router>
   );
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,34 +1,81 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import {Link} from 'react-router-dom'
+import {useSelector} from 'react-redux'
 
-import {Container, Navbar, NavDropdown, Nav, Badge } from 'react-bootstrap'
+import {Container, Navbar, NavDropdown, Nav, Badge} from 'react-bootstrap'
 
-export default function Header(){
-  const { cartItems } = useSelector((state) => state.cart)
+export default function Header() {
+  const {cartItems} = useSelector((state) => state.cart)
+  const {userInfo} = useSelector((state) => state.auth)
+
   const itemsCount = parseInt(cartItems.reduce((acc, item) => acc + item.qty, 0))
-    return (
-        <header>
-            <Navbar bg="dark" variant = "dark" expand="lg" collapseOnSelect="true"  className = "py-3">
-      <Container>
-        <Navbar.Brand><Link to ="/" className = "link-light" style = {{textDecoration:'none'}}>MERN Ecommerce</Link></Navbar.Brand>
+  return (
+    <header>
+      <Navbar
+        bg="dark"
+        variant="dark"
+        expand="lg"
+        collapseOnSelect="true"
+        className="py-3">
+        <Container>
+          <Navbar.Brand>
+            <Link
+              to="/"
+              className="link-light"
+              style={{
+              textDecoration: 'none'
+            }}>MERN Ecommerce</Link>
+          </Navbar.Brand>
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className = "justify-content-end">
-          <Nav className="ml-auto">
-            <Nav.Link><Link to = "/cart" className = "link-light" style = {{textDecoration:'none'}}><i className = 'fas fa-shopping-cart text-secondary'>
-              {
-                cartItems.length > 0 && (
-                  <Badge pill bg='success' className="ml-2">
-                    { itemsCount }
-                  </Badge>
-                ) 
-              }</i></Link></Nav.Link>
-            <Nav.Link><Link to = "/sign-in" className = "link-light" style = {{textDecoration:'none'}}><i className = 'fas fa-user text-secondary'></i> Sign In</Link></Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-        </header>
-    )
+          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <Nav className="ml-auto">
+              <Nav.Link>
+                <Link
+                  to="/cart"
+                  className="link-light"
+                  style={{
+                  textDecoration: 'none'
+                }}>
+                  <i className='fas fa-shopping-cart text-secondary'>
+                    {cartItems.length > 0 && (
+                      <Badge pill bg='success' className="ml-2">
+                        {itemsCount}
+                      </Badge>
+                    )
+}</i>
+                </Link>
+              </Nav.Link>
+
+              {userInfo ? (
+                                             <Nav.Link>
+                                             <Link
+                                               to="/profile"
+                                               className="link-light"
+                                               style={{
+                                               textDecoration: 'none'
+                                             }}>
+                                               <i className='fas fa-user text-secondary'></i>
+                                                <span className = "mx-1">Profile</span></Link>
+                                           </Nav.Link>
+              ) : (
+                              <Nav.Link>
+                              <Link
+                                to="/login"
+                                className="link-light"
+                                style={{
+                                textDecoration: 'none'
+                              }}>
+                                <i className='fas fa-user text-secondary'></i>
+                                Sign In</Link>
+                            </Nav.Link>
+              )}
+
+
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
 }
